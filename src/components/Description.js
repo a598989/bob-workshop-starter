f71ad6ff29e81f73af3608422b6b9c43d9dcc084
a598989 <89257544+a598989@users.noreply.github.com>
2023-10-05 18:30:31 +0200

    Lagt til terning spill

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,26 +1,47 @@
-import React from "react";
-import { Typography, Box } from "@mui/material";
+import React, { useState } from "react";
+import { Typography, Box, Button } from "@mui/material";
 
 function Description() {
+
+  const [diceResult, setDiceResult] = useState(null);
+  const rollDice = () => {
+    const result = Math.floor(Math.random() * 6) + 1; 
+    setDiceResult(result); 
+  };
+
   return (
-    <><Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
-      <Typography variant="h6" gutterBottom color="#3f51b5">
-        This is me
-      </Typography>
-      <Typography>
-        Dett er en side av meg. Det er faktisk meg på bildet der oppe. Assumet du mitt gender?
-        Jeg skårte 4 mot liverpool.
-      </Typography>
-    </Box>
-    
-    <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+    <>
+      <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+        <Typography variant="h6" gutterBottom color="#3f51b5">
+          This is me
+        </Typography>
+        <Typography>
+          Dett er en side av meg. Det er faktisk meg på bildet der oppe. Assumet du mitt gender?
+          Jeg skårte 4 mot Liverpool.
+        </Typography>
+      </Box>
+
+      <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
         <Typography variant="h6" gutterBottom color="#3f51b5">
           Jeg er den beste
         </Typography>
         <Typography>
           William kan ikke lage dette :D
         </Typography>
-      </Box></>
+      </Box>
+
+      {/* Dice roll button and result */}
+      <Box py={3} mt={4} textAlign="center">
+        <Button variant="contained" color="primary" onClick={rollDice}>
+          Roll the Dice
+        </Button>
+        {diceResult !== null && (
+          <Typography variant="h6" mt={2}>
+            You rolled a {diceResult}
+          </Typography>
+        )}
+      </Box>
+    </>
   );
 }
 
