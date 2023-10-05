7d54d91d1b5f3fbf71e16c3cefbe437792a61d3f
a598989 <89257544+a598989@users.noreply.github.com>
2023-10-05 18:06:25 +0200

    Låg til et mellomrom

--- src/components/Description.js
+++ src/components/Description.js
@@ -11,7 +11,9 @@ function Description() {
         Dett er en side av meg. Det er faktisk meg på bildet der oppe. Assumet du mitt gender?
         Jeg skårte 4 mot liverpool.
       </Typography>
-    </Box><Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+    </Box>
+    
+    <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
         <Typography variant="h6" gutterBottom color="#3f51b5">
           Jeg er den beste
         </Typography>
